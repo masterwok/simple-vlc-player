cceb6b1580ce8616d17e4c49fc9ccd9f90ed3a98
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-05-30 23:24:23 -0600

    master - documentation.

--- simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/interfaces/ParamRunnable.java
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/interfaces/ParamRunnable.java
@@ -1,5 +1,18 @@
 package com.masterwok.simplevlcplayer.interfaces;
 
+/**
+ * Simple runnable interface that accepts a generic parameter. Mostly
+ * used to allow for lambda expressions.
+ *
+ * @param <T> The type passed to the run method.
+ */
 public interface ParamRunnable<T> {
+
+    /**
+     * Invoke the function with a parameter of type, T.
+     *
+     * @param t The value parameter of type, T.
+     */
     void run(T t);
+
 }
--- simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/interfaces/ResultCallable.java
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/interfaces/ResultCallable.java
@@ -1,5 +1,17 @@
 package com.masterwok.simplevlcplayer.interfaces;
 
+/**
+ * Simple callable interface that accepts a generic parameter. Mostly
+ * used to allow for lambda expressions.
+ *
+ * @param <T> The type returned from the call method.
+ */
 public interface ResultCallable<T> {
+
+    /**
+     * Invoke the function.
+     *
+     * @return The return value of the function as type, T.
+     */
     T call();
 }

29dd81a96327d47562fee03b74b9e1f78f1ce414
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-06-25 11:30:04 -0400

    v3 - Added resource util method for dimens.

--- simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/utils/ResourceUtil.java
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/utils/ResourceUtil.java
@@ -2,10 +2,12 @@ package com.masterwok.simplevlcplayer.utils;
 
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
 
 /**
  * This class provides static convenience methods for Android resources.
  */
+@SuppressWarnings("WeakerAccess")
 public class ResourceUtil {
 
     private ResourceUtil() {
@@ -18,7 +20,10 @@ public class ResourceUtil {
      * @param id      The id of the color resource.
      * @return The color.
      */
-    public static int getColor(Context context, int id) {
+    public static int getColor(
+            @NonNull Context context,
+            int id
+    ) {
         //noinspection deprecation
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
                 ? context.getResources().getColor(id, null)
@@ -32,7 +37,45 @@ public class ResourceUtil {
      * @param id      The id of the color resource.
      * @return A string resource.
      */
-    public static String getStringResource(Context context, int id) {
+    public static String getString(
+            @NonNull Context context,
+            int id
+    ) {
         return context.getString(id);
+    }
+
+    /**
+     * Get dimension density dependent resource value.
+     *
+     * @param context The context to get the resource from.
+     * @param id      The id of the dimen resource.
+     * @return The density dependent resource value.
+     */
+    public static float getDimen(
+            @NonNull Context context,
+            int id
+    ) {
+        return context
+                .getResources()
+                .getDimension(id);
+    }
+
+    /**
+     * Get dimension as density-independent (dp) value.
+     *
+     * @param context The context to get the resource from.
+     * @param id      The id of the dimen resource.
+     * @return The density-independent resource value.
+     */
+    public static int getDimenDp(
+            @NonNull Context context,
+            int id
+    ) {
+        final float density = context
+                .getResources()
+                .getDisplayMetrics()
+                .density;
+
+        return (int) (getDimen(context, id) / density);
     }
 }

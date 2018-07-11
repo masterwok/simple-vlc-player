86249162bb50fce32113416e47e9985461dd67d7
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-07-11 15:03:23 -0400

    master - Updated sample to support pre KitKat devices.

--- sample/src/main/java/com/masterwok/demosimplevlcplayer/activities/MainActivity.java
+++ sample/src/main/java/com/masterwok/demosimplevlcplayer/activities/MainActivity.java
@@ -3,13 +3,16 @@ package com.masterwok.demosimplevlcplayer.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 
 import com.masterwok.demosimplevlcplayer.R;
 import com.masterwok.simplevlcplayer.VlcOptionsProvider;
 import com.masterwok.simplevlcplayer.activities.MediaPlayerActivity;
+import com.nononsenseapps.filepicker.FilePickerActivity;
 
 
 /**
@@ -74,8 +77,20 @@ public class MainActivity extends AppCompatActivity {
      * Show the activity for picking file.
      */
     private void showOpenDocumentActivity() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.setType("*/*");
+        Intent intent;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Use storage access framework
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.setType("*/*");
+        } else {
+            // Fallback to external file picker.
+            intent = new Intent(this, FilePickerActivity.class);
+            intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+            intent.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
+            intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+            intent.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
+        }
 
         startActivityForResult(intent, OpenDocumentRequestCode);
     }
@@ -106,15 +121,14 @@ public class MainActivity extends AppCompatActivity {
     private void startMediaPlayerActivity(Uri videoUri, Uri subtitleUri) {
         Intent intent = new Intent(this, MediaPlayerActivity.class);
 
-        intent.putExtra(MediaPlayerActivity.MediaUri, videoUri);
-        intent.putExtra(MediaPlayerActivity.SubtitleUri, subtitleUri);
-
+        // Example of using an http resource.
 //        intent.putExtra(MediaPlayerActivity.MediaUri, Uri.parse(
 //                "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
 ////                "http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_stereo.avi"
 //        ));
 
-        // TODO: Seek partial files setTime fails when seeking past downloaded portion of file.
+        intent.putExtra(MediaPlayerActivity.MediaUri, videoUri);
+        intent.putExtra(MediaPlayerActivity.SubtitleUri, subtitleUri);
 
         startActivity(intent);
     }

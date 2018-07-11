af8e5e741bd1be5a97768670a3ac55ce07f9c929
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-07-11 17:51:55 -0400

    master - added getDocumentBitmap util method to BitmapUtil.

--- simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/utils/BitmapUtil.java
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/utils/BitmapUtil.java
@@ -1,9 +1,15 @@
 package com.masterwok.simplevlcplayer.utils;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+
+import java.io.FileNotFoundException;
 
 /**
  * This class provides static convenience methods for bitmaps.
@@ -40,4 +46,38 @@ public class BitmapUtil {
 
         return bitmap;
     }
+
+    /**
+     * Get a thumbnail for the provided document URI.
+     *
+     * @param context            The context to resolve the content resolver from.
+     * @param uri                The URI of the document.
+     * @param width              The width of the thumbnail.
+     * @param height             The height of the thumbnail.
+     * @param cancellationSignal A cancellation signal.
+     * @return If successful, the bitmap. Else, null.
+     */
+    public static Bitmap getDocumentBitmap(
+            Context context,
+            Uri uri,
+            int width,
+            int height,
+            android.os.CancellationSignal cancellationSignal
+    ) {
+        if(width < 0 || height < 0) {
+            return null;
+        }
+
+        try {
+            return DocumentsContract.getDocumentThumbnail(
+                    context.getContentResolver(),
+                    uri,
+                    new Point(width, height),
+                    cancellationSignal
+            );
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
 }

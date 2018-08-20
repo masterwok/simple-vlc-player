36fa93461f229541288f25a591af57f31fe6b3a6
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-08-20 18:21:03 -0400

    subs - Added dependency to coroutines and moved utils to common package.

---
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/common/AndroidJob.kt
@@ -0,0 +1,21 @@
+package com.masterwok.simplevlcplayer
+
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import kotlinx.coroutines.experimental.Job
+
+
+/**
+ * A Kotlin coroutine [@see Job] that cancels itself when the lifecycle it's
+ * bound to is destroyed. This class can be used as a parent job to prevent
+ * memory leaks and null reference exceptions.
+ */
+class AndroidJob(lifecycle: Lifecycle) : Job by Job(), LifecycleObserver {
+    init {
+        lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun destroy() = cancel()
+}

e10ddaf8c6c77975f02981880d171bae38ae1692
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-08-20 18:40:40 -0400

    subs - Documentation.

--- simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/common/extensions/ProgressBarExtensions.kt
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/common/extensions/ProgressBarExtensions.kt
@@ -1,2 +1,13 @@
 package com.masterwok.simplevlcplayer.common.extensions
 
+import android.support.v4.content.ContextCompat
+import android.widget.ProgressBar
+
+
+/**
+ * Set the color of the [ProgressBar] using the provided [colorId].
+ */
+internal fun ProgressBar.setColor(colorId: Int) = indeterminateDrawable.setColorFilter(
+        ContextCompat.getColor(context, colorId)
+        , android.graphics.PorterDuff.Mode.SRC_IN
+)

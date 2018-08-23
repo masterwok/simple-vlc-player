d5f7d95824d398fa74096d2d65b4901f313eab6b
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2018-08-23 00:03:59 -0400

    subs - Converted MainActivity and MediaPlayerActivity to Kotlin.

---
+++ sample/src/main/java/com/masterwok/demosimplevlcplayer/activities/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.masterwok.demosimplevlcplayer.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.support.v7.app.AppCompatActivity
+import com.masterwok.demosimplevlcplayer.R
+import com.masterwok.opensubtitlesandroid.SubtitleLanguage
+import com.masterwok.opensubtitlesandroid.services.OpenSubtitlesService
+import com.masterwok.simplevlcplayer.VlcOptionsProvider
+import com.masterwok.simplevlcplayer.activities.MediaPlayerActivity
+import com.nononsenseapps.filepicker.FilePickerActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val OpenDocumentRequestCode = 32106
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        bindViewComponents()
+        subscribeToViewComponents()
+
+
+        // VlcOptionsProvider can be used to provide LibVlc initialization options.
+        VlcOptionsProvider
+                .getInstance().options = VlcOptionsProvider.Builder(this)
+                .setVerbose(true)
+                .build()
+    }
+
+    /**
+     * Bind view components to private fields.
+     */
+    private fun bindViewComponents() {
+        //        buttonPlay = findViewById(R.id.button_play);
+    }
+
+    /**
+     * Subscribe to any bound view components.
+     */
+    private fun subscribeToViewComponents() {
+        buttonPlay.setOnClickListener { view ->
+            // Disable play button (prevent bounce/re-enabled in onStart())
+            view.isEnabled = false
+
+            showOpenDocumentActivity()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Re-enable play button (prevent bounce)
+        buttonPlay.isEnabled = true
+    }
+
+    /**
+     * Show the activity for picking file.
+     */
+    private fun showOpenDocumentActivity() {
+        val intent: Intent
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // Use storage access framework
+            intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                type = "*/*"
+            }
+        } else {
+            // Fallback to external file picker.
+            intent = Intent(this, FilePickerActivity::class.java).apply {
+                putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)
+                putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false)
+                putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE)
+                putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().path)
+            }
+        }
+
+        startActivityForResult(intent, OpenDocumentRequestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != OpenDocumentRequestCode
+                || resultCode != Activity.RESULT_OK
+                || data ==
+                null) {
+            return
+        }
+
+        startMediaPlayerActivity(data.data, null)
+    }
+
+    /**
+     * Start the simple-vlc-player media player activity. Subtitle must be local
+     * file Uri as it appears libVLC does not support adding subtitle using a
+     * FileDescriptor (like Media instance).
+     *
+     * @param videoUri    The selected video URI.
+     * @param subtitleUri The selected subtitle URI (must be local file URI).
+     */
+    private fun startMediaPlayerActivity(videoUri: Uri?, subtitleUri: Uri?) =
+            startActivity(Intent(this, MediaPlayerActivity::class.java).apply {
+
+                putExtra(MediaPlayerActivity.MediaUri, videoUri)
+                putExtra(MediaPlayerActivity.SubtitleUri, subtitleUri)
+                putExtra(MediaPlayerActivity.SubtitleDestinationUri, Uri.fromFile(cacheDir))
+
+                // This should be the User-Agent you registered with opensubtitles.org
+                // See: http://trac.opensubtitles.org/projects/opensubtitles/wiki/DevReadFirst
+                putExtra(MediaPlayerActivity.OpenSubtitlesUserAgent, OpenSubtitlesService.TemporaryUserAgent)
+                putExtra(MediaPlayerActivity.SubtitleLanguageCode, SubtitleLanguage.English)
+            })
+}
---
+++ simplevlcplayer/src/main/java/com/masterwok/simplevlcplayer/activities/MediaPlayerActivity.kt
@@ -0,0 +1,115 @@
+package com.masterwok.simplevlcplayer.activities
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.content.LocalBroadcastManager
+import com.masterwok.simplevlcplayer.R
+import com.masterwok.simplevlcplayer.dagger.injectors.InjectableAppCompatActivity
+import com.masterwok.simplevlcplayer.fragments.BasePlayerFragment
+import com.masterwok.simplevlcplayer.fragments.LocalPlayerFragment
+import com.masterwok.simplevlcplayer.fragments.RendererPlayerFragment
+import com.masterwok.simplevlcplayer.services.MediaPlayerService
+
+class MediaPlayerActivity : InjectableAppCompatActivity() {
+
+    companion object {
+        const val MediaUri = BasePlayerFragment.MediaUri
+        const val SubtitleUri = BasePlayerFragment.SubtitleUri
+        const val SubtitleDestinationUri = BasePlayerFragment.SubtitleDestinationUri
+        const val OpenSubtitlesUserAgent = BasePlayerFragment.OpenSubtitlesUserAgent
+        const val SubtitleLanguageCode = BasePlayerFragment.SubtitleLanguageCode
+    }
+
+    var localPlayerFragment: LocalPlayerFragment? = null
+    var rendererPlayerFragment: RendererPlayerFragment? = null
+
+    private val broadCastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action ?: return
+
+            when (action) {
+                MediaPlayerService.RendererClearedAction -> showLocalPlayerFragment()
+                MediaPlayerService.RendererSelectionAction -> showRendererPlayerFragment()
+            }
+        }
+    }
+
+    val mediaPlayerServiceIntent: Intent
+        get() = Intent(
+                applicationContext,
+                MediaPlayerService::class.java
+        )
+
+
+    private fun showLocalPlayerFragment() {
+        rendererPlayerFragment = null
+        localPlayerFragment = LocalPlayerFragment()
+        showFragment(localPlayerFragment!!)
+    }
+
+    private fun showRendererPlayerFragment() {
+        localPlayerFragment = null
+        rendererPlayerFragment = RendererPlayerFragment()
+        showFragment(rendererPlayerFragment!!)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_media_player)
+
+        if (savedInstanceState != null) {
+            return
+        }
+
+        showLocalPlayerFragment()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        registerRendererBroadcastReceiver()
+
+        startService(mediaPlayerServiceIntent)
+    }
+
+    override fun onBackPressed() {
+        // Always ensure that we stop the media player service when navigating back.
+        stopService(mediaPlayerServiceIntent)
+
+        super.onBackPressed()
+    }
+
+    private fun registerRendererBroadcastReceiver() {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(MediaPlayerService.RendererClearedAction)
+        intentFilter.addAction(MediaPlayerService.RendererSelectionAction)
+
+        LocalBroadcastManager
+                .getInstance(this)
+                .registerReceiver(broadCastReceiver, intentFilter)
+    }
+
+    override fun onStop() {
+        LocalBroadcastManager
+                .getInstance(this)
+                .unregisterReceiver(broadCastReceiver)
+
+        rendererPlayerFragment = null
+
+        super.onStop()
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.framelayout_fragment_container, fragment)
+                .commit()
+    }
+
+
+}
